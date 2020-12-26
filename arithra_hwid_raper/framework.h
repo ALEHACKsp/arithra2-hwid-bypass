6fe11a59237d1c5db7ac037dda6332fcf2a339aa
hash <kxhashimoto@gmail.com>
2020-12-26 20:54:57 +0100

    Add project files.

---
+++ arithra_hwid_raper/framework.h
@@ -0,0 +1,12 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <MinHook.h>
+#pragma comment(lib, "libMinHook.lib")
---
+++ arithra_hwid_raper/main.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch.h"
+
+typedef char* (__fastcall* t_get_hwid)();
+t_get_hwid o_get_hwid;
+bool did_init = false;
+std::string rand_hwid = "";
+
+std::string gen_random(const int len) 
+{
+    std::string tmp_s;
+    static const char alphanum[] = "0123456789" "ABCDEF";
+    srand((unsigned)time(NULL) * GetCurrentProcessId());
+    tmp_s.reserve(len);
+    for (int i = 0; i < len; ++i) tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    return tmp_s;
+}
+
+char* f_get_hwid()
+{
+    auto r = o_get_hwid();
+    printf("client attempting to get hwid, sending crap (orig: %s)\n", r);
+    return (char*)rand_hwid.c_str();
+}
+
+void start()
+{
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+    SetConsoleTitleA("lel!");
+    std::cout << "hooking" << std::endl;
+    auto base = GetModuleHandleA("Arithra2-Client.exe");
+    printf("based in: %p func in: %p\n", base, 0x143130 + base);
+
+    rand_hwid = gen_random(36);
+
+    printf("random hwid will be %s\n", rand_hwid.c_str());
+
+    BYTE b1 = 0, b2 = 0;
+
+    b1 = *(BYTE*)(0x683130);
+    b2 = *(BYTE*)(0x683130 + 1);
+
+    printf("b1: %02x %02x\n", b1, b2);
+
+
+    /*
+    .text:00543130
+    .text:00543130 ; Attributes: bp-based frame
+    .text:00543130
+    .text:00543130 get_hwid_mixed  proc near               ; CODE XREF: sub_552260+BD↓p
+    .text:00543130
+    */
+    MH_Initialize();
+    MH_CreateHook((void*)(0x683130), (void*)f_get_hwid, (void**)&o_get_hwid);
+    MH_EnableHook((void*)(0x683130));
+
+    b1 = *(BYTE*)(0x683130);
+    b2 = *(BYTE*)(0x683130 + 1);
+
+    printf("b1: %02x %02x\n", b1, b2);
+
+
+    printf("hooked shit\n");
+    did_init = true;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH && !did_init) CreateThread(0, 0, (LPTHREAD_START_ROUTINE)start, 0, 0, 0);
+    return TRUE;
+}
+
